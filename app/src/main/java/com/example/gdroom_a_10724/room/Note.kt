4970990a4e9e86009d6b773fadcbc823cec0e978
rztt01* <vinsariest@gmail.com>
2022-09-19 13:26:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gdroom_a_10724/room/Note.kt
@@ -0,0 +1,11 @@
+package com.example.gdroom_a_10724.room
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val note: String
+    )
---
+++ app/src/main/java/com/example/gdroom_a_10724/room/NoteDB.kt
@@ -0,0 +1,37 @@
+package com.example.gdroom_a_10724.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import java.security.AccessControlContext
+
+@Database(
+    entities = [Note::class],
+    version = 1
+)
+
+abstract class NoteDB: RoomDatabase() {
+
+    abstract fun noteDao() : NoteDao
+
+    companion object {
+
+        @Volatile private var instance : NoteDB? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?:
+synchronized(LOCK) {
+            instance ?: buildDatabase(context).also {
+                instance = it
+            }
+        }
+
+        private fun buildDatabase(context: Context) =
+Room.databaseBuilder(
+            context.applicationContext,
+            NoteDB::class.java,
+            "note12345.db"
+        ).build()
+    }
+}
